97a723a5dcebbdb791c225d0bdec06ad9ab8a73a
Gideon kirui <kipyegongideon99@gmail.com>
2024-03-17 16:47:53 +0300

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee = {
+    name: "Gideon Kirui",
+    streetAddress: "123 Main St"
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+console.log("Before update:", employee);
+
+destructivelyUpdateEmployeeWithKeyAndValue(employee, "position", "Senior Manager");
+console.log("After update:", employee);
+
+
+function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = Object.assign({}, employee);
+    delete clonedEmployee[key];
+    return clonedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
